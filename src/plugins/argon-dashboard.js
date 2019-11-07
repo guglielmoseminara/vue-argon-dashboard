735bacea1ab8571e9811edae8094ca24c4f7fb37
guglielmoseminara <guglielmo.seminara@gmail.com>
2019-11-07 12:57:12 +0100

    fix(styles): added style import

--- src/plugins/argon-dashboard.js
+++ src/plugins/argon-dashboard.js
@@ -1,3 +1,5 @@
+import "../assets/vendor/nucleo/css/nucleo.css";
+import "../assets/scss/argon.scss";
 import globalComponents from "./globalComponents";
 import globalDirectives from "./globalDirectives";
 import SidebarPlugin from "../components/SidebarPlugin/index"
